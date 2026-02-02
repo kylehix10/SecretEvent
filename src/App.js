b5fdc8836cffa71778445005175d8fab34ce177e
KyleHix <khix@email.sc.edu>
2026-02-02 18:59:15 -0500

    testSlot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Countdown from './CountdownTimer.jsx'
+import testSlot from './images/testSlot.png'
 
 function App() {
   const launchDate = new Date("2027-02-19T23:59:59");
@@ -8,6 +9,8 @@ function App() {
     <div class = 'app'>
       <h2>Caption</h2>
       <Countdown targetDate={launchDate} />
+      <img src={testSlot} alt="test"></img>
+      
     </div>
   );
 }
